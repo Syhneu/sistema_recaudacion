8832bf19229d214c77bb02d23874b51c67728dab
Yancarlo Urdaneta <yancarlou@gmail.com>
2020-10-16 11:11:38 -0400

    arreglando conexion bdd

--- sistema_recaudacion/sistema_recaudacion/settings.py
+++ sistema_recaudacion/sistema_recaudacion/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'sistema_recaudacion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shapiro_rentas_marino_2020',
+        'USER': 'postgres',
+        'PASSWORD': 'mateo30.',
+        'HOST': 'localhost',
+        'PORT': '3000',
     }
 }
 
