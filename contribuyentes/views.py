e52fd88413dc8c28b382879db0754e850c7d2e82
Yushen <y_loz@hotmail.com>
2020-10-19 09:41:11 -0400

    django makemigrations

--- contribuyentes/views.py
+++ contribuyentes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .forms import ContribuyentesForm
+from .models import RcContrib
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+class CreateContribuyente(generic.CreateView):
+    model = RcContrib
+    template = 'contribuyentes/registro.html'
+    form_class = ContribuyentesForm
