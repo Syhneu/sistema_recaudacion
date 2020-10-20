c3aedfc20d72562623e428a30d0474a8dcb387b1
Yushen <y_loz@hotmail.com>
2020-10-20 09:40:54 -0400

    cambiando la Class Based View a Function Based View

--- contribuyentes/urls.py
+++ contribuyentes/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'contribuyentes'
 urlpatterns = [
-    path('', views.CreateContribuyente.as_view(template_name='contribuyentes/registro.html'), name='registro')
+    path('', views.registrar_contribuyente, name='registro')
 ]
--- contribuyentes/views.py
+++ contribuyentes/views.py
@@ -5,7 +5,26 @@ from .models import RcContrib, RcAccionistas, RcRepresentante
 
 # Create your views here.
 
-class CreateContribuyente(generic.CreateView):
-    model = RcContrib
-    template = 'registro.html'
-    form_class = ContribuyentesForm
+#class CreateContribuyente(generic.CreateView):
+#    model = RcContrib
+#    template = 'registro.html'
+#    form_class = ContribuyentesForm
+
+# Probando con Function Based View
+def registrar_contribuyente(request):
+
+    if request.method == 'POST':
+        pass
+
+        # if valido
+        # save
+        
+        # else
+        # print error
+    else:
+        pass
+
+    return render(request, 'contribuyentes/registro.html', {
+        # formulario1: nombre_formulario,
+        # formulario2: nombre_formulario,
+    })
